ad1aea0a4eaaee0a238d80e3d505ff728b7af1fc
Jlaborne <laborne.j@hotmail.fr>
2024-04-17 12:13:59 +0000

    added shell.c files containing the main entry of the program

---
+++ shell.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the program.
+ *        Prompts the user for input and processes commands.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	char *tmp = malloc(1024);
+	size_t len = 1024;
+
+	while (1)
+	{
+		printf("> ");
+		getline(&tmp, &len, stdin);
+		command = args_betty(tmp);
+	}
+	return (0);
+}
