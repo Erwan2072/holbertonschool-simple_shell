68f1f1d9a2b1d7a4efbb13847ef5f740cd44f4c6
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-18 10:14:26 +0200

    shell.h

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifdef SHELL_H
+#define __MAIN_H__
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv);
+char **args_betty(char *user_input);
+
+#endif
