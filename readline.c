3c76c3aad3b645149b52ef9bfb749200e7a76a9b
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-18 13:03:24 +0200

    readline.c

---
+++ readline.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+char readline(char *line)
+{
+    char *tmp = NULL;
+	size_t len = 0;
+
+	while(1)
+	{
+		if (getline(&tmp, &len, stdin) == -1)
+            if (feof(stdin)) /*la fin du fichier d'entrée a été atteinte (EOF - End Of File)*/
+                exit(EXIT_SUCCESS);/*we recieved an EOF*/
+    }
+    else 
+    {
+        perror("readline")
+        exit (EXIT_FAILURE);
+    }
+	return(line);
+}
