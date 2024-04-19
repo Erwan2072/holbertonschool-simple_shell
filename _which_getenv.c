be57b8e0ee9f08739707707816a0c0cbda12fb54
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-19 10:35:53 +0200

    _which_getenv.c

---
+++ _which_getenv.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+
+
+/**
+ * _which - Find and print the full path of a given executable file in PATH
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 if file found, -1 otherwise
+ */
+int _which(int argc, char *argv[])
+{
+    char *path = getenv("PATH");
+    if (path == NULL)
+    {
+        printf("Error: PATH environment variable not set.\n");
+        return (-1); // Erreur si la variable d'environnement PATH n'est pas définie
+    }
+
+    // Tokenize the PATH variable
+    char *token = strtok(path, ":");
+    while (token != NULL)
+    {
+        // Check each directory for the specified file
+        for (int i = 1; i < argc; i++)
+        {
+            char filepath[1024];
+            sprintf(filepath, "%s/%s", token, argv[i]);
+            // Check if the file exists and is executable
+            if (access(filepath, F_OK | X_OK) == 0)
+            {
+                printf("%s\n", filepath);
+                return (0); // Fichier trouvé, retourne 0
+            }
+        }
+        token = strtok(NULL, ":");
+    }
+
+    return (-1); // Fichier non trouvé, retourne -1
+}
+
+/**
+ * _getenv - Find the environment variable
+ * @name: Environment variable name
+ * @env: Array of environment variables
+ * Return: Pointer to the content of the environment variable, NULL if not found
+ */
+char *_getenv(const char *name, char **env)
+{
+    int i, j;
+
+    for (i = 0; env[i]; i++)
+    {
+        for (j = 0; env[i][j]; j++)
+        {
+            if (env[i][j] == name[j])
+                continue;
+            else if (env[i][j] == '=' && name[j] == '\0')
+            {
+                return (env[i] + j + 1); // Retourne le contenu de la variable d'environnement
+            }
+            else
+                break;
+        }
+    }
+    return (NULL); // Variable d'environnement non trouvée, retourne NULL
+}
