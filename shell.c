7e203fff78c24249b907cb34aa11aa42154479fd
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-22 10:03:03 +0200

    correction  shell.c

--- shell.c
+++ shell.c
@@ -14,27 +14,27 @@ int main(int argc, char **argv, char **env)
 
     if (argc > 1)
     {
-        // Mode non interactif : arguments de ligne de commande fournis
-        tmp = argv[1]; // Utilisez le premier argument comme commande
+        /*Mode non interactif : arguments de ligne de commande fournis*/
+        tmp = argv[1]; /*Utilisez le premier argument comme commande*/
         command = parsing_args(tmp);
         execute(command, env);
     }
     else
     {
-        // Mode interactif : pas d'arguments de ligne de commande
+        /*Mode interactif : pas d'arguments de ligne de commande*/
         while (1)
         {
-            if (isatty(STDIN_FILENO)) // Vérifie si le programme est en mode interactif
-                printf("($) "); // Affiche le prompt
-            getline(&tmp, &len, stdin); // Lire la commande depuis l'entrée standard
+            if (isatty(STDIN_FILENO))/*Vérifie si le prog est> mode interactif*/
+                printf("($) "); /*Affiche le prompt*/
+            getline(&tmp, &len, stdin);/*Lire la commande depuis stdin*/
             command = parsing_args(tmp);
             execute(command, env);
-            free(tmp); // Libérer la mémoire allouée par getline
-            tmp = NULL; // Réinitialiser tmp pour éviter les fuites de mémoire
+            free(tmp); /*Libérer la mémoire allouée par getline*/
+            tmp = NULL; /*Réinitialiser tmp pour éviter les fuites de mémoire*/
         }
     }
 
-    // Libérer la mémoire
+    /*Libérer la mémoire*/
     free(tmp);
     free(command);
 
