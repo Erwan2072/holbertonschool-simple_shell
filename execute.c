dc51a335473e74f561c6204b39d63fc99ef4b582
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-23 13:47:32 +0000

    ajout exit status in execute.c

--- execute.c
+++ execute.c
@@ -2,46 +2,33 @@
 
 /**
  * execute - command executor
- * @cmd_t: =1 if args[0] is a executable©, 0otherwise
  * @args: An array of char str the arguments of the command to be executed
  * @env: An array of str the environment in which the command is to be executed
- * @cmd_count: counter of the number of times a command has been executed
- * @av: - function name, e.g. ./hsh
  *Return: always 0
  */
-int execute(int cmd_t, char **args, char **env, int cmd_count, char *av)
+int execute(char **args, char **env)
 {
 	pid_t child_pid;
 	int status;
+	char *file_path;
 
-	if (cmd_t == NULL)/*if no executable found*/
-	{
-		printf(strerr, "%s: not found\n", av, cmd_count, args[0]);
-		return (127);
-	}
-	else
-	{
-		child_pid = fork();/*creation process child */
-
-		/* if child process creation fails */
-		if (child_pid == -1)
+	file_path = _which(args[0], env);
+		if (file_path == NULL)
 		{
-			perror("fork");
+			printf("%s: not found\n", args[0]);
 			return (-1);
 		}
-		/*child process*/
-		if (child_pid == 0)
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+		if (execve(file_path, args, env) == -1)
 		{
-			/* Exec cmd with arguments, then if successful, close the process */
-			if (execve(args[0], args, env) == -1)
-			{
-				/*If Exec fails, prints msg err and closes child process */
-				printf(stderr, "%s: %d: %s: not found\n", av, cmd_count, args[0]);
-				exit(127);
-			}
+			printf("erreur exec \n");
+			exit(127);
 		}
-		/*parent process */
-		else if (child_pid > 0)
+	}
+	else if (child_pid < 0)
+	{
 		{
 			wait(&status);/*Waits for end of child process and recovers process status*/
 			if ((WEXITSTATUS(status)) != 0)/*if child process status not null */
@@ -49,6 +36,5 @@ int execute(int cmd_t, char **args, char **env, int cmd_count, char *av)
 			return (status);
 		}
 	}
-	printf(stderr, "%s: %d: %s: not found\n", av, cmd_count, args[0]);
 	return (127);
 }
