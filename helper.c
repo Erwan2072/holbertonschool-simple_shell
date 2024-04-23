9e42a6facc6680de7e60ba900af92422fad1421f
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-23 09:18:49 +0000

    ajout copy fonction modif

---
+++ helper.c
@@ -0,0 +1,90 @@
+#include "shell.h"
+
+/**
+ * _strdup - duplicate a string using malloc
+ *@str: string to duplicate
+ *Return: Pointer to the new string
+ */
+
+char *_strdup(char *str)
+{
+	char *d;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	d = malloc(sizeof(char) * (len + 1));
+
+	if (d == NULL)
+		return (NULL);
+
+	while ((d[i] = str[i]) != '\0')
+		i++;
+
+	return (d);
+}
+
+/**
+ *_strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
+
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
