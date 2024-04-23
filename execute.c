46a0b109556ad629262a65cd81abaa0502a89103
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-23 13:36:57 +0000

    ajout exit status in execute.c

--- execute.c
+++ execute.c
@@ -2,38 +2,53 @@
 
 /**
  * execute - command executor
+ * @cmd_t: =1 if args[0] is a executable©, 0otherwise
  * @args: An array of char str the arguments of the command to be executed
  * @env: An array of str the environment in which the command is to be executed
+ * @cmd_count: counter of the number of times a command has been executed
+ * @av: - function name, e.g. ./hsh
  *Return: always 0
  */
-int execute(char **args, char **env)
+int execute(int cmd_t, char **args, char **env, int cmd_count, char *av)
 {
 	pid_t child_pid;
 	int status;
-	char *file_path;
 
-	file_path = _which(args[0], env);
-		if (file_path == NULL)
-		{
-			printf("%s: not found\n", args[0]);
-			return (-1);
-		}
-	child_pid = fork();
-	if (child_pid == 0)
+	if (cmd_t == NULL)/*if no executable found*/
 	{
-		if (execve(file_path, args, env) == -1)
-		{
-			printf("erreur exec \n");
-			exit(-1);
-		}
-	}
-	else if (child_pid < 0)
-	{
-		printf("erreur fork \n");
-		return (-1);
+		printf(strerr, "%s: not found\n", av, cmd_count, args[0]);
+		return (127);
 	}
 	else
-		wait(&status);
+	{
+		child_pid = fork();/*creation process child */
 
-	return (0);
+		/* if child process creation fails */
+		if (child_pid == -1)
+		{
+			perror("fork");
+			return (-1);
+		}
+		/*child process*/
+		if (child_pid == 0)
+		{
+			/* Exec cmd with arguments, then if successful, close the process */
+			if (execve(args[0], args, env) == -1)
+			{
+				/*If Exec fails, prints msg err and closes child process */
+				printf(stderr, "%s: %d: %s: not found\n", av, cmd_count, args[0]);
+				exit(127);
+			}
+		}
+		/*parent process */
+		else if (child_pid > 0)
+		{
+			wait(&status);/*Waits for end of child process and recovers process status*/
+			if ((WEXITSTATUS(status)) != 0)/*if child process status not null */
+				status = (WEXITSTATUS(status)); /*retrieve child process status */
+			return (status);
+		}
+	}
+	printf(stderr, "%s: %d: %s: not found\n", av, cmd_count, args[0]);
+	return (127);
 }
