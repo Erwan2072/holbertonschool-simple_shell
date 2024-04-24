35383d796f6df496ed43730ceced8c23d9185165
Jlaborne <laborne.j@hotmail.fr>
2024-04-24 11:24:17 +0000

    switched to the dev version of execute.c

--- execute.c
+++ execute.c
@@ -1,40 +1,40 @@
 #include "shell.h"
 
-/**
- * execute - command executor
- * @args: An array of char str the arguments of the command to be executed
- * @env: An array of str the environment in which the command is to be executed
- *Return: always 0
- */
 int execute(char **args, char **env)
 {
 	pid_t child_pid;
-	int status;
+	int status, i;
 	char *file_path;
 
+	printf("Command to execute: ");
+	for(i = 0; args[i] != NULL; i++)
+	{
+		printf("%s ", args[i]);
+	}
+	printf("\n");
 	file_path = _which(args[0], env);
 		if (file_path == NULL)
 		{
-			printf("%s: not found\n", args[0]);
+			printf("No such file or directory\n");
 			return (-1);
 		}
+	printf("file_path: %s\n", file_path);
 	child_pid = fork();
 	if (child_pid == 0)
 	{
 		if (execve(file_path, args, env) == -1)
 		{
 			printf("erreur exec \n");
-			exit(127);
+			return (-1);
 		}
 	}
 	else if (child_pid < 0)
 	{
-		{
-			wait(&status);/*Waits for end of child process and recovers process status*/
-			if ((WEXITSTATUS(status)) != 0)/*if child process status not null */
-				status = (WEXITSTATUS(status)); /*retrieve child process status */
-			return (status);
-		}
+		printf("erreur fork \n");
+		return (-1);
 	}
-	return (127);
+	else
+		wait(&status);
+
+	return (0);
 }
